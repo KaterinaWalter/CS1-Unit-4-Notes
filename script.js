e8fed0a631577ed75b92740683184b141528b6cb
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2026-04-07 13:40:25 +0000

    started notes

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-// This is a SINGLE LINE COMMENT (note)
-
-/* This is a MULTI LINE COMMENT
-   Code below is an example JS STATEMENT
-   (instruction/command) 
-   Every statement ends in a SEMICOLON
-*/
-console.log("welcome to javascript!");
+console.log("hello world");
 
 
 
