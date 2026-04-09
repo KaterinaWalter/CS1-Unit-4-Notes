fea45c504beb750c5d007620c1b1dee0d3f1fc0a
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2026-04-09 15:43:55 +0000

    variables

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 // This is a SINGLE LINE COMMENT
 
 /* This is a MULTI LINE COMMENT
-The code below is an example STATEMENT (instruction/command)
-Statements end with a SEMICOLON
-*/
-console.log("Welcome to JS!");
+The code below is an example STATEMENT
+(instruction/command)
+Every statement ends with a SEMICOLON
+ */
+console.log("welcome to javascript!");
 
-/* A VARIABLE is a named storage
-container for data (information) 
-*/
+// VARIABLES are named storage containers for data (info)
 
-// DECLARE a new variable (make space, give it a name/label)
-let username;
+// DECLARE a variable with the keyword LET
+let username; 
 
-// ASSIGN a value (data) to the var
-username = "ms. walter";
+// ASSIGN a value to a named variable
+username = "ms.walter";
 
-// see the value stored in the var
+// DECLARE + ASSIGN a new variable in one line
+let pokemon = "pikachu";
+
+// See the value stored in a variable
 console.log(username);
+console.log(pokemon);
 
-// DECLARE + ASSIGN in one step
+// Store numbers in a variable
 let luckyNum = 13;
-
-
